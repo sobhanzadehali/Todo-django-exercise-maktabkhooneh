f73904d5cd21585c6efd2fc4790cab1cf97a6c6f
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-04-25 00:30:46 -0700

    drf_yasg added and works for tasks

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,9 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="TODO app apis",
+      default_version="v1",
+      description="this api documentation is for maktabkhooneh homework",
+      terms_of_service="",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include('rest_framework.urls')),
     path('accounts/', include('accounts.urls')),
     path('todo/', include('todo.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
