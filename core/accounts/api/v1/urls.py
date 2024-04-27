8f574ee5c16eb73dd858759be573de2b54a81ea7
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-04-27 09:18:24 -0400

    +djoser urls

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('', include('djoser.urls')),
-    path('', include('djoser.urls.jwt')),
+    path('', include('djoser.urls')), # without jwt
+    path('', include('djoser.urls.jwt')), #with jwt
     
 ]
