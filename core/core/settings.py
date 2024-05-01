242c610854efc1ae1410db7f3e7dfa693f8d6877
Sobhan Zadehali <99137872+sobhanzadehali@users.noreply.github.com>
2024-05-01 03:47:06 -0400

    postgresql added and works well with celery
    problem is now we have to migrate and createsuper user in backend container shell by hand.

--- core/core/settings.py
+++ core/core/settings.py
@@ -85,12 +85,25 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# sqlite database
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# postgresql database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": config("SQL_ENGINE", default="django.db.backends.sqlite3"),
+        "NAME": config("SQL_DATABASE"),
+        "USER": config("SQL_USER"),
+        "PASSWORD": config("SQL_PASSWORD"),
+        "HOST": config("SQL_HOST"),
+        "PORT": config("SQL_PORT"),
     }
 }
+
 
 
 # Password validation
